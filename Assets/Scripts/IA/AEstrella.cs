ab88a93a19afaffb41c49d5885488f9ae0a1e7e5
Laraman97 <laraman97@gmail.com>
2024-11-10 19:32:31 -0300

    Update AEstrella.cs

--- Assets/Scripts/IA/AEstrella.cs
+++ Assets/Scripts/IA/AEstrella.cs
@@ -25,20 +25,35 @@ public class AEstrella : MonoBehaviour
      **/public int Heuristica(LevelNode node)
     {
         int h = 1000000000;
-        List<Vector2> boxes;
-        List<Vector2> goals;
+        List<Vector2> boxes = new List<Vector2>();
+        List<Vector2> goals = new List<Vector2>();
         for (int i = 0; i<node.matrixValues.GetLength(0); i++) 
         {
             for (int j = 0; j < node.matrixValues.GetLength(1); j++)
             {
-                if (node.matrixValues[i,j]== ) 
-                {
-
-                }
+                if (node.matrixValues[i, j] == 3)
+                    boxes.Add(new Vector2(i, j));
+                if (node.matrixValues[i, j] == 4)
+                    goals.Add(new Vector2(i, j));
             }
         }
+        if (goals.Count != boxes.Count) 
+        {
+            return h;
+            Debug.Log("Error en la Heuristica\nDiferente Nº de cajas que metas");
+        }
+        int nFact = boxes.Count;
+        int i = boxes.Count;
+        while (i>1) 
+        {
+            i--;
+            nFact *= i;
+        }
+
         return h;
     }
+
+    
 
     // Start is called before the first frame update
     void Start()
